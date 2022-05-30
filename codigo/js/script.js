50d9a4e5fb2317a0ccf8d3085869503173f0884d
joaogabriels <joaogabrielsevangelista@gmail.com>
2022-05-29 21:37:11 -0300

    mudança no código

--- codigo/js/script.js
+++ codigo/js/script.js
@@ -1,69 +1,42 @@
-const postsContainer = document.querySelector("#receitas-container")
+const receitasContainer = document.querySelector("#receitas-container")
 const loadercontainer = document.querySelector(".loader")
-const filterInput = document.querySelector('#filter')
+const searchBar = document.getElementById("searchBar")
 
-let page = 1
-
+searchBar.addEventListener('input', (e) =>{
+    console.log(e.target.value)
+})
 
 const getRecipe = async() => {
     const response = await fetch('https://api.jsonbin.io/b/6292583b402a5b3802120ace')
     const data = await response.json()
+    displayRecipe(data);
     return data;
 }
 
-const generateRecipetemplate = recipe => recipe.map(item => `
-    <div class="post">
-        <div class="card-receita">
-            <h3>${item.nome}</h3>
-            <div class="box-receita">
-                <p>${item.resumo}</p>
-                <ul>
-                    <li><i class="fa-solid fa-house-chimney"></i>${item.tempo_de_preparo}</li>
-                    <li><i class="fa-solid fa-user"></i>${item.porcoes}</li>
-                    <li><i class="fa-solid fa-heart"></i>Favorito</li>
-                </ul>
-            </div>
-            <div class="link">
-                <a href=""><i class="fa-solid fa-angle-right"></i> Mão na massa</a>
-            </div>
-        </div>
-    </div>
-    `).join("")
-
-const addPostsInToDOM = async() => {
-    const recipes = await getRecipe()
+const displayRecipe = ( recipes ) => {
     console.log(recipes)
-    const recipesTemplate = generateRecipetemplate(recipes)
-    postsContainer.innerHTML += recipesTemplate
+    const recipeBox = recipes
+        .map((recipe) => {
+            return `
+            <div class="post">
+                <div class="card-receita">
+                    <h3>${recipe.nome}</h3>
+                    <div class="box-receita">
+                        <p>${recipe.resumo}</p>
+                        <ul>
+                            <li><i class="fa-solid fa-house-chimney"></i>${recipe.tempo_de_preparo}</li>
+                            <li><i class="fa-solid fa-user"></i>${recipe.porcoes}</li>
+                            <li><i class="fa-solid fa-heart"></i>Favorito</li>
+                        </ul>
+                    </div>
+                    <div class="link">
+                        <a href=""><i class="fa-solid fa-angle-right"></i> Mão na massa</a>
+                    </div>
+                </div>
+            </div>
+            `
+        }).join("")
+    receitasContainer.innerHTML +=  recipeBox;
 }
-
-addPostsInToDOM()
-const getNextsPosts = () => {
-    setTimeout(() => {
-        page++;
-        addPostsInToDOM();
-    }, 300);
-
-}
-const removeLoader = () => {
-    setTimeout(() => {
-        loadercontainer.classList.remove("show")
-        getNextsPosts();
-    }, 1000)
-}
-const showLoader = () => {
-    loadercontainer.classList.add("show")
-    removeLoader();
-}
-
-const handleScrollToPageBottom = () => {
-    const { clientHeight, scrollHeight, scrollTop } = document.documentElement
-    const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10
-    if (isPageBottomAlmostReached) {
-        showLoader();
-    }
-}
-
-window.addEventListener('scroll', handleScrollToPageBottom)
-
+    
 getRecipe()
